168591a741396f32f83292109909aa689ac59614
Inviles <oleg_inv@mail.ru>
2019-05-23 10:47:05 +0400

    get opened windows

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const { desktopCapturer } = require('electron');
+
+desktopCapturer.getSources( { types: ['window'] } )
+    .then( windows => {
+        console.log( windows );
+
+        const div = document.querySelector('.windows');
+
+        windows.forEach( ( window, index ) => {
+            const windowDiv = document.createElement('div');
+            windowDiv.classList.add('window-object')
+            windowDiv.innerHTML = `${index + 1}: ${JSON.stringify( window, null, 4 ) }`;
+            div.appendChild( windowDiv );
+        } );
+
+        // div.innerHTML = JSON.stringify( windows, null, 4 );
+    } )
+    .catch( err => console.error( err ) );
